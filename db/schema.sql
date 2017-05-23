9b7e6def2f8ea27a2d6ce9759ccfee37079fda31
Joe Mulick <joemulick@gmail.com>
2017-05-22 20:09:00 -0700

    Minor updates for heroku. App up and running on heroku.

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE burger_db;
-USE burger_db;
+USE g3ze7wiay2i44m1p;
 
 CREATE TABLE burger_table(
 
@@ -8,5 +7,9 @@ CREATE TABLE burger_table(
     devoured BOOLEAN DEFAULT false,
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
-    
+
 );
+
+INSERT INTO burger_table (burger_name,devoured) VALUES ('chicken burger',false);
+INSERT INTO burger_table (burger_name,devoured) VALUES ('bacon burger',false);
+INSERT INTO burger_table (burger_name,devoured) VALUES ('turkey burger',false);
